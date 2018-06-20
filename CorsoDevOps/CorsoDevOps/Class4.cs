9ca404f502279c36f2f42a9892d9ebbe1148bea2
ITALY\eperaino <edoardo.peraino@eng.it>
2018-06-20 10:04:48 +0200

    commit

---
+++ CorsoDevOps/CorsoDevOps/Class4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorsoDevOps
+{
+    public class Calculator
+    {
+        public int ADD(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtrack (int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public int Divide(int a, int b)
+        {
+            
+            {
+
+                if (b == 0)
+                    throw new ArgumentException("Divisione per zero");
+                return a / b;
+
+            }
+
+            
+        }
+   }
+}
